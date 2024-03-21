acdee14d17a7da5a912ea50e0594eb9dfa5b305e
bananaOP <118002518+bananaOP@users.noreply.github.com>
2024-03-21 22:24:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-07a645b1-f740-4075-8066-401e27efefc2/)
 
 
 echo "$curl_output" >> Update.tsx
